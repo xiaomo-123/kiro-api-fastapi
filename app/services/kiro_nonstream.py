66848590b5ed613c11fabf7c59f3ac11658b262d
xiaomo-123 <2763349475@qq.com>
2026-01-04 17:15:03 +0800

    v4.0.1 add kiro-network

--- app/services/kiro_nonstream.py
+++ app/services/kiro_nonstream.py
@@ -399,11 +399,10 @@ class KiroNonStreamService(KiroBaseService):
             'Authorization': f'Bearer {self.access_token}',
             'amz-sdk-invocation-id': str(uuid.uuid4()),
             "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36",
+            "Accept": "application/json, */*",
             "Accept-Language": "en-US,en;q=0.9",
-            "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
-            "Accept-Encoding": "gzip, deflate, br",
-            "Connection": "keep-alive",
-            "Upgrade-Insecure-Requests": "1"
+            "Content-Type": "application/json",
+            "Connection": "keep-alive"
         }
 
         try:
